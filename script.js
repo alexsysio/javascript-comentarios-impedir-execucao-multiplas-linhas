6b95a52b5b7e1a8892547e0734fe0dea8d2ea267
Alexsandro <alexsandrolt1996@gmail.com>
2026-08-11 16:29:04 -0300

    JavaScript Comentários impedir execucacao de múltiplas linhas

---
+++ script.js
@@ -0,0 +1,5 @@
+/* document.getElementById('myH').innerHTML = 'Bem-vindo à minha página inicial';
+document.getElementById('myP').innerHTML = 'Isso é meu primeiro parágrafo';
+*/
+document.getElementById('myP').innerHTML =
+  'O bloco de comentários não é executado.';
